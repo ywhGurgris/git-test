e77363c329f0a6e0c10d9fdc54ed793902568003
ywh <1450476215@qq.com>
2022-11-15 19:43:33 +0800

    git test first commit

--- main/java/com/it/gittest.java
+++ main/java/com/it/gittest.java
@@ -3,5 +3,6 @@ package com.it;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git.sou");
+        System.out.println("hello git.sou2");
     }
 }
